ee4ed8463bfd23db9e652e9b3b9cca06521703f6
oda <ooddaa@gmail.com>
2022-09-29 10:09:16 +0100

    day 1 done

---
+++ elixir/day_01/part-02.exs
@@ -0,0 +1,11 @@
+
+"./day-01_data"
+|> File.stream!()
+|> Stream.map(&String.replace(&1, "\n", ""))
+|> Stream.map(&String.to_integer/1)
+|> Stream.chunk_every(3,1, :discard)
+|> Stream.chunk_every(2,1, :discard)
+# |> Enum.map(&(&1))
+# |> Enum.take(10)
+|> Enum.count(fn [left, right] -> Enum.sum(left) < Enum.sum(right) end)
+|> IO.inspect() # 1589
